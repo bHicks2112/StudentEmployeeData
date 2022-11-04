4e9cd0c80f27097cc4dcff52052ef84c26f352cc
bHicks2112 <79944750+bHicks2112@users.noreply.github.com>
2022-11-03 18:50:47 -0600

    udpate notifications

--- StudentEmployeeData/Models/Notification.cs
+++ StudentEmployeeData/Models/Notification.cs
@@ -8,6 +8,45 @@ namespace StudentEmployeeData.Models
 {
     public class Notification
     {
+        public List<NotificationItem> Items {get ; set;} = new List<NotificationItem>(); 
+        // e-form not submitted notification 
+        foreach (var item in Employee)
+        {
+            if ((item.employee.SubmittedEForm == 'no') && (DateTime.Today > Convert.ToDateTime(item.employee.AuthorizationToWorkEmailSentDate).AddDays(7)) {
+                Items.Add(new NotificationItem
+                {
+                    Type = 'E-form not submitted'
+                    Message = 'This student has not submitted their e-form.'
+                    EmployeeId = item.EmployeeId
+                })
+            })
+        }
+// increase pay rate notification
+        foreach (var item in Employee)
+        {
+            if (Datetime.Today > Convert.ToDateTime(item.employee.IncreaseInputDate).AddMonths(4) {
+                Items.Add(new NotificationItem
+                {
+                    Type = 'Pay Increase'
+                    Message = 'Increase pay rate.'
+                    EmployeeId = item.EmployeeId
+                })
+            } ) 
+    }
+            foreach (var item in Employee)
+        {
+            if ((item.employee.AuthorizationToWorkReceived == 'No') && (DateTime.Today > Convert.ToDateTime(item.employee.EFormSubmissionDate).AddDays(7)) {
+                Items.Add(new NotificationItem
+                {
+                    Type = 'Authorization to Work'
+                    Message = 'Reminder to follow up with student about authorization to work.'
+                    EmployeeId = item.EmployeeId
+                })
+            } ) 
+    }
+
+    public class NotificationItem
+    {
         [Key]
         [Required]
         public int NotificationId { get; set; }
